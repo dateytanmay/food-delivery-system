c5f6f140a29900f6cf4d653355aa92b7f0c55519
Tanmay Datey <tanmay.datey123@gmail.com>
2025-09-10 02:11:24 +0530

    add code again

--- assessment/assessment/src/main/java/com/limetray/assessment/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.limetray.assessment.repository;
-
-import com.limetray.assessment.Entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
